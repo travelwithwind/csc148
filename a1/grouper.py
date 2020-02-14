477ca52d32fb0cfffe234b3e5ddf5a5e2fee4444
148zhuanyong <60999835+148zhuanyong@users.noreply.github.com>
2020-02-14 18:56:25 -0500

    Add files via upload

--- a1/grouper.py
+++ a1/grouper.py
@@ -143,11 +143,11 @@ class AlphaGrouper(Grouper):
         Hint: the sort_students function might be useful
         """
         # TODO: complete the body of this method
-        
+
         #sort the student by name
         sorted_stu = sort_students(course.get_students(), 'name')
         sliced_list = slice_list(sorted_stu, self.group_size)
-        g =Grouping()
+        g = Grouping()
         for batch in sliced_list:
             temp_group = Group(batch)
             g.add_group(temp_group)
@@ -184,7 +184,7 @@ class RandomGrouper(Grouper):
         random.shuffle(stu)
         sorted_stu = stu
         sliced_list = slice_list(sorted_stu, self.group_size)
-        g =Grouping()
+        g = Grouping()
         for batch in sliced_list:
             temp_group = Group(batch)
             g.add_group(temp_group)
@@ -232,21 +232,21 @@ class GreedyGrouper(Grouper):
         # TODO: complete the body of this method
         stu = list(course.get_students())
         stu_size = len(stu)
-        
+
         g = Grouping()
-        
+
         while stu != []:
 
-            if (stu_size - len(stu))%self.group_size== 0:
-                first_stu =stu.pop(0)
+            if (stu_size - len(stu))%self.group_size == 0:
+                first_stu = stu.pop(0)
                 new_group = Group([first_stu])
 
             if self.group_size == 1:
                 g.add_group(new_group)
                 continue
 
-            curr_max_score= -1000
-            
+            curr_max_score = -1000
+
             for candidate in stu:
                 test_group = new_group.get_members()+[candidate]
                 score = survey.score_students(test_group)
@@ -254,22 +254,22 @@ class GreedyGrouper(Grouper):
                     curr_max_score = score
                     curr_best_candidate = candidate
             if stu:
-                new_group._members.append(curr_best_candidate)
+                new_group.get_members().append(curr_best_candidate)
                 stu.remove(curr_best_candidate)
 
-            if len( new_group._members) == self.group_size:
+            if len(new_group.get_members()) == self.group_size:
                 g.add_group(new_group)
 
         if 0 < len(new_group) <= self.group_size:
             g.add_group(new_group)
-        
+
         return g
-    
-            
-            
-                    
-                
-        
+
+
+
+
+
+
 
 
 class WindowGrouper(Grouper):
@@ -321,26 +321,26 @@ class WindowGrouper(Grouper):
         while len(stu) > self.group_size:
             wds = windows(stu, self.group_size)
             for i in range(len(wds)):
-                
+
                 curr_score1 = survey.score_students(wds[i])
-                
-                if i ==len(wds)-1:
+
+                if i == len(wds)-1:
                     idx_next = 0
                 else:
                     idx_next = i+1
                 curr_score2 = survey.score_students(wds[idx_next])
-                
+
                 if curr_score1 >= curr_score2:
                     g.add_group(Group(wds[i]))
-                    
+
                     #remove students that are assigned group from the stu list
                     for s in wds[i]:
                         stu.remove(s)
                     break
-                
+
         if 0 < len(stu) <= self.group_size:
             g.add_group(Group(stu))
-        
+
         return g
 
 class Group:
@@ -367,8 +367,8 @@ class Group:
         if len(set(ids)) != len(ids):
             print('violation of Representation Invariants\
                   :No two students in _members have the same id')
-            return 
-        
+            return
+
         self._members = members
 
     def __len__(self) -> int:
@@ -382,7 +382,7 @@ class Group:
         as <member>.
         """
         # TODO: complete the body of this method
-        ids = [stu.id for stu in self._members ]
+        ids = [stu.id for stu in self._members]
         return member.id in ids
 
     def __str__(self) -> str:
@@ -393,7 +393,7 @@ class Group:
         You can choose the precise format of this string.
         """
         # TODO: complete the body of this method
-        names = [stu.name for stu in self._members]      
+        names = [stu.name for stu in self._members]
         return str(names)
 
     def get_members(self) -> List[Student]:
@@ -426,7 +426,7 @@ class Grouping:
         """ Return the number of groups in this grouping """
         # TODO: complete the body of this method
         return len(self._groups)
-    
+
     def __str__(self) -> str:
         """
         Return a multi-line string that includes the names of all of the members
@@ -436,11 +436,11 @@ class Grouping:
         You can choose the precise format of this string.
         """
         # TODO: complete the body of this method
-        output =''
+        output = ''
         for group in self._groups:
             output += str(group)+'\n'
         return output
-        
+
 
     def add_group(self, group: Group) -> bool:
         """
@@ -450,25 +450,25 @@ class Grouping:
         invariant don't add it and return False instead.
         """
         # TODO: complete the body of this method
-        
-            
+
+
         #No group in _groups contains zero members
-        if group._members == []:
+        if group.get_members() == []:
             return False
-        
+
         #No student appears in more than one group in _groups
         for stu in group.get_members():
             for g in self._groups:
                 if stu.id in [s.id for s in g.get_members()]:
                     return False
-                
+
         self._groups.append(group)
         return True
-        
+
 
     def get_groups(self) -> List[Group]:
-        """ Return a list of all groups in this grouping. 
-        This list should be a shallow copy of the self._groups 
+        """ Return a list of all groups in this grouping.
+        This list should be a shallow copy of the self._groups
         attribute.
         """
         # TODO: complete the body of this method
