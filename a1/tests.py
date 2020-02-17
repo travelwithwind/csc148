007a2d10f7338b261a0f4babdb035f70638ef20e
travelwithwind <oddsnends@163.com>
2020-02-17 11:55:35 -0500

    修bug

--- a1/tests.py
+++ a1/tests.py
@@ -34,7 +34,6 @@ class TestStudent:
 
 class TestCourse:
 
-
     def test_enroll_students(self) -> None:
         # successfully enrolled
         course1 = Course("CSC148")
@@ -67,7 +66,7 @@ class TestCourse:
 class TestMultipleChoiceQuestion:
     def test_validate_answer(self) -> None:
         # content is Multiple Choice
-        multi1 = MultipleChoiceQuestion\
+        multi1 = MultipleChoiceQuestion \
             (1, "Multiple Choice Question 1", ["A", "B", "C", "D"])
         answer1 = Answer("A")
         assert multi1.validate_answer(answer1)
@@ -78,7 +77,7 @@ class TestMultipleChoiceQuestion:
             (1, "Multiple Choice Question 1", ["A", "B", "C", "D"])
         answer1 = Answer("A")
         answer2 = Answer("A")
-        assert self.get_similarity(answer1.content, answer2.content) \
+        assert multi1.get_similarity(answer1, answer2) \
                == 1.0
 
 
@@ -96,7 +95,7 @@ class TestNumericQuestion:
         numeric1 = NumericQuestion(1, "Numeric1", 0, 5)
         answer1 = Answer(1)
         answer2 = Answer(1)
-        assert self.get_similarity(self.answer1.content, self.answer2.content) \
+        assert numeric1.get_similarity(answer1, answer2) \
                == 1.0
 
 
@@ -114,7 +113,7 @@ class TestYesNoQuestionQuestion:
         yesno1 = YesNoQuestion(1, "YesNo1")
         answer1 = Answer(True)
         answer2 = Answer(True)
-        assert self.get_similarity(answer1.content, answer2.content) \
+        assert yesno1.get_similarity(answer1, answer2) \
                == 1.0
 
 
@@ -132,7 +131,7 @@ class TestCheckboxQuestion:
         checkbox1 = CheckboxQuestion(1, "Checkbox", ["A", "B", "C", "D"])
         answer1 = Answer("A")
         answer2 = Answer("A")
-        assert self.get_similarity(answer1.content, answer2.content) \
+        assert checkbox1.get_similarity(answer1, answer2) \
                == 1.0
 
 
@@ -176,7 +175,7 @@ class LonelyMemberCriterion:
         choices = "A"
         c = criterion.LonelyMemberCriterion()
         question1 = MultipleChoiceQuestion(1, "Multi1", "A")
-        assert self.c.score_answers(self.question11, [Answer(self.choices)]) \
+        assert c.score_answers(question1, [Answer(choices)]) \
                == 0
 
 
@@ -201,7 +200,6 @@ class TestGrouping:
         assert len(group.get_groups()) == 1
 
 
-
 class TestSurvey:
     def test_get_questions(self) -> None:
         student = course.Student(1, "CSC148")
@@ -216,15 +214,13 @@ class TestSurvey:
         student.set_answer(question, survey.Answer(True))
         sur = survey.Survey([])
 
-
-
     def test_get_weight(self) -> None:
         student = course.Student(1, "CSC148")
         question = survey.YesNoQuestion(1, "YesNo")
         student.set_answer(question, survey.Answer(True))
         sur = survey.Survey([question])
         sur.set_weight(5, question)
-        assert question.get_weight() == 5
+        assert sur._get_weight(question) == 5
 
     def test_score_student(self) -> None:
         student = course.Student(1, "CSC148")
@@ -239,12 +235,11 @@ class TestSurvey:
         assert sur.score_grouping(grouper.Grouping()) == 0
 
 
-
-def test_slice_list(self) -> None:
+def test_slice_list() -> None:
     assert slice_list([1, 2, 3, 4, 5], 2) == [[1, 2], [3, 4], [5]]
 
 
-def windows(self) -> None:
+def windows() -> None:
     assert windows([1, 2, 3], 2) == [[1, 2], [2, 3]]
 
 
@@ -258,6 +253,7 @@ class TestAlphaGrouper:
         group1 = grouper.AlphaGrouper(1).make_grouping(course1, sur)
         assert len(group1) == 5
 
+
 class TestRandomGrouper:
     def test_make_grouping(self) -> None:
         course1 = Course("CSC148")
@@ -268,6 +264,7 @@ class TestRandomGrouper:
         group1 = grouper.RandomGrouper(1).make_grouping(course1, sur)
         assert len(group1) == 5
 
+
 class TestGreedyGrouper:
     def test_make_grouping(self) -> None:
         course1 = Course("CSC148")
@@ -277,6 +274,7 @@ class TestGreedyGrouper:
         course1.enroll_students(student)
         group1 = grouper.GreedyGrouper(1).make_grouping(course1, sur)
         assert len(group1) == 5
+
 
 class TestWindowGrouper:
     def test_make_grouping(self) -> None:
@@ -289,7 +287,5 @@ class TestWindowGrouper:
         assert len(group1) == 5
 
 
-
 if __name__ == '__main__':
-
     pytest.main(['tests.py'])
